2fe4e3d1f9ade3c16228bdf7c7d675374a6ba309
BbeiyLily <baltimore.leslie@gmail.com>
2018-11-08 22:46:36 +0100

    script fonctionnel

---
+++ script.py
@@ -0,0 +1,34 @@
+
+# import libraries
+import urllib2
+from bs4 import BeautifulSoup
+
+import csv
+from datetime import datetime
+
+# specify the url
+quote_page = 'https://www.jennyfer.com/fr-fr/vetements/pulls-et-gilets/'
+
+# query the website and return the html to the variable page
+page = urllib2.urlopen(quote_page)
+
+# parse the html using beautiful soup and store in variable soup
+soup = BeautifulSoup(page, 'html.parser')
+
+# Take out the <div> of name and get its value
+name_box = soup.find('a', attrs={'class': 'user-login'})
+
+#After we have the tag, we can get the data by getting its text.
+name = name_box.text.strip() # strip() is used to remove starting and trailing
+print name
+
+# get the index price
+#price_box = soup.find('div', attrs={'class':'priceText__1853e8a5'})
+#price = price_box.text
+#print price
+
+
+# open a csv file with append, so old data will not be erased
+with open('index.csv', 'a') as csv_file:
+ writer = csv.writer(csv_file)
+ writer.writerow([name, datetime.now()])
