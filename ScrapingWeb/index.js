8082695fae1a06f00c5809f92dedfd919007538e
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-12-02 14:46:57 +0100

    Promise

--- ScrapingWeb/index.js
+++ ScrapingWeb/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const {promisify} = require('util');
+const { promisify } = require('util');
 const schedule = require('node-schedule');
 
 
@@ -10,85 +10,90 @@ const readFileAsync = promisify(fs.readFile);
 function recuperationListeVetement() {
     axios.get('https://www.jennyfer.com/fr-fr/vetements/pulls-et-gilets/')
         .then((response) => {
-            if(response.status === 200) {
+            if (response.status === 200) {
                 const html = response.data;
                 const $ = cheerio.load(html);
                 let listPull = [];
                 let priceDateArray = [];
                 let currentDate = Date(Date.now());
-                $('#search-result-items .list-tile').each(function(i, elem) {
+                $('#search-result-items .list-tile').each(function (i, elem) {
                     listPull[i] = {
                         id: $(this).find('.product-wrapper').data('product-id'),
                         title: $(this).find('.product-name h3').text().trim(),
                         url: $(this).find('.img-link').attr('href'),
-                        image : $(this).find('.img-link img').attr('src'),
+                        image: $(this).find('.img-link img').attr('src'),
                         priceD: priceDateArray[0] = [
-                            [ $(this).find('.price').text().trim(), currentDate],
-                            [ "30euros", "lundi"]
+                            [$(this).find('.price').text().trim(), currentDate],
+                            ["30euros", "lundi"]
                         ]
                     }
                 });
                 console.log(listPull);
-                const listPullTrimmed = listPull.filter(n => n != undefined )
+                const listPullTrimmed = listPull.filter(n => n != undefined)
                 fs.writeFile('listPull.json',
                     JSON.stringify(listPullTrimmed, null, 4),
-                    (err)=> console.log('File successfully written!'))
+                    (err) => console.log('File successfully written!'))
             }
-        }, (error) => console.log(err) );
+        }, (error) => console.log(err));
 }
 
 function updateListeVetement() {
     axios.get('https://www.jennyfer.com/fr-fr/vetements/pulls-et-gilets/')
         .then((response) => {
-            if(response.status === 200) {
+            if (response.status === 200) {
                 const html = response.data;
                 const $ = cheerio.load(html);
                 let listPullUpdate = [];
                 let priceDateArray = [];
                 let currentDate = Date(Date.now());
-                $('#search-result-items .list-tile').each(function(i, elem) {
+                $('#search-result-items .list-tile').each(function (i, elem) {
                     listPullUpdate[i] = {
                         id: $(this).find('.product-wrapper').data('product-id'),
                         title: $(this).find('.product-name h3').text().trim(),
                         url: $(this).find('.img-link').attr('href'),
-                        image : $(this).find('.img-link img').attr('src'),
+                        image: $(this).find('.img-link img').attr('src'),
                         priceD: priceDateArray[0] = [
-                            [ $(this).find('.price').text().trim(), currentDate],
-                            [ "30euros", "lundi"]
+                            [$(this).find('.price').text().trim(), currentDate],
+                            ["30euros", "lundi"]
                         ]
                     }
                 });
 
 
                 async function getListPullMain() {
-                        try {
-                            let ListPullMain = JSON.parse(await fs.readFileAsync('listPull.json', {encoding: 'utf8'}));
-                            console.log('CONTENT:', text);
-                        } catch (error) {
-                            console.log('ERROR:', error);
-                        } 
+                    return readFileAsync('listPull.json', 'utf8');
                 }
-                console.log(ListPullMain);
+
+                try {
+                    getListPullMain().then(function (resultat) {
+                        let data = JSON.parse(JSON.stringify(resultat));
+
+                        listPullUpdate.forEach(function (item) {
+                            let idItemUpdate = item["id"];
+                            console.log(idItemUpdate);
+                            console.log(data);
+                            
+                            data.forEach(function(itemMain) {
+                                if (idItemUpdate == itemMain["id"]) {
+                                    console.log(" id pareil");
+                                } else {
+                                    console.log("id different");
+                                }
+                            });
+
+                        });
 
 
-                listPullUpdate.forEach(function(item) {
-                   
-                    let idItemUpdate = item["id"];
-                    ListPullMain.forEach(function(itemMain) {
-                       if(idItemUpdate == itemMain["id"]) {
-                           console.log(" id pareil");
-                       } else {
-                           console.log("id different");
-                       }
+                        /*const listPullUpdateTrimmed = listPullUpdate.filter(n => n != undefined )
+                        fs.writeFile('listPullUpdate.json',
+                            JSON.stringify(listPullUpdateTrimmed, null, 4),
+                            (err)=> console.log('File successfully written!'))*/
                     });
-                });
-
-                /*const listPullUpdateTrimmed = listPullUpdate.filter(n => n != undefined )
-                fs.writeFile('listPullUpdate.json',
-                    JSON.stringify(listPullUpdateTrimmed, null, 4),
-                    (err)=> console.log('File successfully written!'))*/
+                } catch (error) {
+                    console.log(error);
+                }
             }
-        }, (error) => console.log(err) );
+        }, (error) => console.log(err));
 }
 
 //recuperationListeVetement();
