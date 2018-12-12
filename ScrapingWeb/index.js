bd0dadee339135a7b5a9ab4f731ccaba1b2e6c92
Leslie Baltimore <lbaltimore@kookline.net>
2018-12-12 14:26:03 +0100

    nettoyage du code

--- ScrapingWeb/index.js
+++ ScrapingWeb/index.js
@@ -23,8 +23,8 @@ function recuperationListeVetement() {
                         url: $(this).find('.img-link').attr('href'),
                         image: $(this).find('.img-link img').attr('src'),
                         priceD: priceDateArray[0] = [
-                            [$(this).find('.price').text().trim(), currentDate],
-                            ["30euros", "lundi"]
+                            { obj : $(this).find('.price').text().trim(), currentDate},
+                    
                         ]
                     }
                 });
@@ -37,63 +37,49 @@ function recuperationListeVetement() {
         }, (error) => console.log(err));
 }
 
-function updateListeVetement() {
-    axios.get('https://www.jennyfer.com/fr-fr/vetements/pulls-et-gilets/')
-        .then((response) => {
-            if (response.status === 200) {
-                const html = response.data;
-                const $ = cheerio.load(html);
-                let listPullUpdate = [];
-                let priceDateArray = [];
-                let currentDate = Date(Date.now());
-                $('#search-result-items .list-tile').each(function (i, elem) {
-                    listPullUpdate[i] = {
-                        id: $(this).find('.product-wrapper').data('product-id'),
-                        title: $(this).find('.product-name h3').text().trim(),
-                        url: $(this).find('.img-link').attr('href'),
-                        image: $(this).find('.img-link img').attr('src'),
-                        priceD: priceDateArray[0] = [
-                            [$(this).find('.price').text().trim(), currentDate],
-                            ["30euros", "lundi"]
-                        ]
-                    }
-                });
+async function updateListeVetement() {
+    const response = await axios.get('https://www.jennyfer.com/fr-fr/vetements/pulls-et-gilets/').catch(error => console.log(err));
+    if (response.status !== 200) return;
+    
+    const html = response.data;
+    const $ = cheerio.load(html);
+    let listPullUpdate = [];
+    let priceDateArray = [];
+    let currentDate = Date(Date.now());
+    $('#search-result-items .list-tile').each(function (i, elem) {
+        listPullUpdate[i] = {
+            id: $(this).find('.product-wrapper').data('product-id'),
+            title: $(this).find('.product-name h3').text().trim(),
+            url: $(this).find('.img-link').attr('href'),
+            image: $(this).find('.img-link img').attr('src'),
+            priceD: priceDateArray[0] = [
+                {obj : $(this).find('.price').text().trim(), currentDate}
+            ]
+        }
+    });
 
-
-                async function getListPullMain() {
-                    return readFileAsync('listPull.json', 'utf8');
-                }
-
-                try {
-                    getListPullMain().then(function (resultat) {
-                        let data = [];
-                         data = JSON.parse(JSON.stringify(resultat));
-                         
-                        listPullUpdate.forEach(function(item) {
-                            let idItemUpdate = item["id"];
-                            
-                            for (var itemPullMain in data) {
-                                console.log(itemPullMain);
-                                
-                                /*if (idItemUpdate == itemPullMain.id ) {
-                                    console.log(" id pareil");
-                                } else {
-                                    console.log("id different");
-                                }*/
-                            }
-                        });
-
-
-                        /*const listPullUpdateTrimmed = listPullUpdate.filter(n => n != undefined )
-                        fs.writeFile('listPullUpdate.json',
-                            JSON.stringify(listPullUpdateTrimmed, null, 4),
-                            (err)=> console.log('File successfully written!'))*/
-                    });
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        }, (error) => console.log(err));
+    const resultat = await readFileAsync('listPull.json', 'utf8').catch(error => console.log(err));
+    // transforme la liste en objet
+    let data = JSON.parse(resultat);
+    
+    // pour chaque pull de la liste à jour...
+    listPullUpdate.forEach(function(item) {
+        //...on récupère l'id
+        let idItemUpdate = item["id"];
+        const byId = el => el.id === idItemUpdate;
+        const currentItem = data.find(byId);
+        if  (item.priceD[item.priceD.length - 1 ].obj  == currentItem.priceD[0].obj ) {
+            console.log("meme prix ");
+        } else {
+            console.log("different");
+            item.priceD.push(currentItem.priceD[0]);
+        }
+        
+    });
+    /*const listPullUpdateTrimmed = listPullUpdate.filter(n => n != undefined )
+    fs.writeFile('listPullUpdate.json',
+        JSON.stringify(listPullUpdateTrimmed, null, 4),
+        (err)=> console.log('File successfully written!'))*/
 }
 
 //recuperationListeVetement();
