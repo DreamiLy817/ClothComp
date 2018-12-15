6fe9f8ba7e022d617313fb256467d5959dd720c9
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-12-15 17:04:37 +0100

    suppression fonction recuperationListe

--- ScrapingWeb/index.js
+++ ScrapingWeb/index.js
@@ -7,46 +7,39 @@ const schedule = require('node-schedule');
 
 const readFileAsync = promisify(fs.readFile);
 
-function recuperationListeVetement() {
-
-    axios.get('https://www.jennyfer.com/fr-fr/vetements/pulls-et-gilets/')
-        .then((response) => {
-            if (response.status === 200) {
-                const html = response.data;
-                const $ = cheerio.load(html);
-                let listPull = [];
-                let priceDateArray = [];
-                let currentDate = Date(Date.now());
-                $('#search-result-items .list-tile').each(function (i, elem) {
-                    listPull[i] = {
-                        id: $(this).find('.product-wrapper').data('product-id'),
-                        title: $(this).find('.product-name h3').text().trim(),
-                        url: $(this).find('.img-link').attr('href'),
-                        image: $(this).find('.img-link img').attr('src'),
-                        priceD: priceDateArray[0] = [
-                            { prix : $(this).find('.price').text().trim(), currentDate},
-                    
-                        ]
-                    }
-                });
-                console.log(listPull);
-                const listPullTrimmed = listPull.filter(n => n != undefined)
-                fs.writeFile('listPull.json',
-                    JSON.stringify(listPullTrimmed, null, 4),
-                    (err) => console.log('File successfully written!'))
-            }
-        }, (error) => console.log(err));
-}
-
-async function updateListeVetement() {
+async function ListeVetement() {
     const response = await axios.get('https://www.jennyfer.com/fr-fr/vetements/pulls-et-gilets/').catch(error => console.log(err));
     if (response.status !== 200) return;
+    
+    const listPullStockeStr = await readFileAsync(__dirname+'/listPull.json', 'utf8').catch(error => console.log(error));
+
     
     const html = response.data;
     const $ = cheerio.load(html);
     let listPullUpdate = [];
     let priceDateArray = [];
+    let listPull = [];
     let currentDate = Date(Date.now());
+
+    if(!listPullStockeStr) {
+        $('#search-result-items .list-tile').each(function (i, elem) {
+            listPull[i] = {
+                id: $(this).find('.product-wrapper').data('product-id'),
+                title: $(this).find('.product-name h3').text().trim(),
+                url: $(this).find('.img-link').attr('href'),
+                image: $(this).find('.img-link img').attr('src'),
+                priceD: priceDateArray[0] = [
+                    { prix : $(this).find('.price').text().trim(), currentDate},
+            
+                ]
+            }
+        });
+        const listPullTrimmed = listPull.filter(n => n != undefined)
+        fs.writeFile('listPull.json',
+            JSON.stringify(listPullTrimmed, null, 4),
+            (err) => console.log('La liste de pull de base a été enregistré'))
+            return;
+    } 
     $('#search-result-items .list-tile').each(function (i, elem) {
         listPullUpdate[i] = {
             id: $(this).find('.product-wrapper').data('product-id'),
@@ -59,7 +52,7 @@ async function updateListeVetement() {
         }
     });
 
-    const listPullStockeStr = await readFileAsync(__dirname+'/listPull.json', 'utf8').catch(error => console.log(error));
+    //const listPullStockeStr = await readFileAsync(__dirname+'/listPull.json', 'utf8').catch(error => console.log(error));
     // transforme la liste en objet
     let listPullStocke = JSON.parse(listPullStockeStr);
     
@@ -85,9 +78,7 @@ async function updateListeVetement() {
     const listPullUpdateTrimmed = listPullStocke.filter(n => n != undefined )
     fs.writeFile('listPullUpdateNewPull.json',
         JSON.stringify(listPullUpdateTrimmed, null, 4),
-        (err)=> console.log('File successfully written!'))
+        (err)=> console.log('La mise a jour de la liste a bien été effectué'))
 }
 
-//recuperationListeVetement();
-
-updateListeVetement();
+ListeVetement();
